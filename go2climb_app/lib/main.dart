0694f5a64073286c2f7bbb9dafb1ab55b18a8b99
Pierix <89226508+PieroMarquez@users.noreply.github.com>
2024-11-22 02:27:44 -0500

    Update main.dart

--- go2climb_app/lib/main.dart
+++ go2climb_app/lib/main.dart
@@ -1,3 +1,5 @@
+import 'dart:io';
+
 import 'package:flutter/material.dart';
 import 'Screens/home.dart'; // Importa la pantalla Home
 import 'Screens/account.dart';
@@ -24,7 +26,54 @@ class Go2ClimbApp extends StatelessWidget {
         '/account': (context) => AccountScreen(),
         '/profile': (context) => ProfileScreen(),
         '/devices': (context) => DeviceScreen(),
+        '/logout': (context) => LogoutScreen(),
       },
+    );
+  }
+}
+class LogoutScreen extends StatefulWidget {
+  @override
+  _LogoutScreenState createState() => _LogoutScreenState();
+}
+
+class _LogoutScreenState extends State<LogoutScreen> {
+  @override
+  void initState() {
+    super.initState();
+    _scheduleLogout();
+  }
+
+  void _scheduleLogout() {
+    // Espera 3 segundos antes de cerrar la aplicación
+    Future.delayed(Duration(seconds: 3), () {
+      exit(0); // Cierra la aplicación
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Color(0xFF223240),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Image.asset(
+              'lib/assets/logo.png', // Asegúrate de tener el logo en esta ruta
+              height: 100,
+            ),
+            SizedBox(height: 20),
+            Text(
+              'See you soon',
+              style: TextStyle(
+                color: Colors.white,
+                fontSize: 24.0,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
